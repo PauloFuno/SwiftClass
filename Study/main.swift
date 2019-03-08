cb3fae817514cacf330ffb2e09edbc9260ef90c3
pauloschmidt <paulosergiofuno@gmail.com>
2019-03-08 13:55:07 -0800

    Initial Commit

---
+++ Study/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Study
+//
+//  Created by Paulo Schmidt on 2019-03-08.
+//  Copyright © 2019 Paulo Schmidt. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
